ed2fea7dbf4ce0d2bbd33c56e843cf614c95522e
Unknown <marko13431@gmail.com>
2019-03-24 19:18:55 +0200

    Added User Stories
    Check if ok

--- AprioriClose/src/MainApp.java
+++ AprioriClose/src/MainApp.java
@@ -1,11 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MainApp {
 
