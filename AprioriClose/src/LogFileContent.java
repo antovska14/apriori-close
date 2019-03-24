fc337b1ed90e685a16a0fccddb8e586260b14a83
antovska14 <antovska14@gmail.com>
2019-03-24 15:15:14 +0200

    1.1

--- AprioriClose/src/LogFileContent.java
+++ AprioriClose/src/LogFileContent.java
@@ -4,6 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LogFileContent {
 	private String dateTime;
@@ -13,8 +15,8 @@ public class LogFileContent {
 	private String description;
 	private String origin;
 	private String IPAddress;
-	
-	public LogFileContent(String [] values) {
+
+	public LogFileContent(String[] values) {
 		this.dateTime = values[0];
 		this.eventContext = values[1];
 		this.component = values[2];
@@ -24,34 +26,46 @@ public class LogFileContent {
 		this.IPAddress = values[6];
 	}
 
-	public String getDateTime() {
-		return dateTime;
-	}
-
-	public String getEventContext() {
-		return eventContext;
-	}
-
-	public String getComponent() {
-		return component;
-	}
-
-	public String getEventName() {
-		return eventName;
-	}
-
 	public String getDescription() {
 		return description;
 	}
 
-	public String getOrigin() {
-		return origin;
+	public static List<LogFileContent> readLogFile() throws IOException {
+		List<LogFileContent> list = new ArrayList<>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("logs_BCS37_20181103.csv"));
+			String line;
+			int counter = 0;
+			boolean first = true;
+			while ((line = br.readLine()) != null && counter < 100) {
+				if (!first) {
+					line = line.replaceFirst(",", "");
+				}
+				first = false;
+				String[] values = line.split(",");
+				LogFileContent logFileLine = new LogFileContent(values);
+				list.add(logFileLine);
+				counter++;
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
 	}
 
-	public String getIPAddress() {
-		return IPAddress;
+	public static List<Integer> mapDescription(String description) {
+		List<Integer> descriptionContent = new ArrayList<Integer>();
+		String contentPattern = "[0-9]+";
+
+		Pattern pattern = Pattern.compile(contentPattern);
+		Matcher matcher = pattern.matcher(description);
+
+		while (matcher.find()) {
+			descriptionContent.add(Integer.parseInt(matcher.group(0)));
+		}
+
+		return descriptionContent;
 	}
-	
-	
-	
+
 }
--- AprioriClose/src/MainApp.java
+++ AprioriClose/src/MainApp.java
@@ -8,68 +8,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainApp {
-	
-	
-	public static void main(String[] args) {		
+
+	public static void main(String[] args) {
 		List<LogFileContent> logFileList = new ArrayList<LogFileContent>();
-		List<List<Integer>> transactionsTable = new ArrayList<List<Integer>>(); 
+		List<List<Integer>> transactionsTable = new ArrayList<List<Integer>>();
 		try {
-			logFileList = readLogFile();
+			logFileList = LogFileContent.readLogFile();
 			transactionsTable = mapToTransactionTable(logFileList);
 			System.out.println(transactionsTable.toString());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static List<LogFileContent> readLogFile() throws IOException {
-		List<LogFileContent> list = new ArrayList<>();
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("logs_BCS37_20181103.csv"));
-			String line;
-			int counter = 0;
-			boolean first = true;
-			while((line = br.readLine())!=null && counter < 100) {
-				if(!first) {
-					line = line.replaceFirst(",", "");
-				}
-				first = false;
-				String[] values = line.split(",");
-				LogFileContent logFileLine = new LogFileContent(values); 
-				list.add(logFileLine);
-				counter++;
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return list;
-	}
-	
-	public static List<List<Integer>> mapToTransactionTable(List<LogFileContent> logFileList){
+
+	public static List<List<Integer>> mapToTransactionTable(List<LogFileContent> logFileList) {
 		List<List<Integer>> transactionTable = new ArrayList<List<Integer>>();
-		
-		for(int i = 1 ; i < logFileList.size(); i++) {
-			List<Integer> descriptionContent = mapDescription(logFileList.get(i).getDescription());
+
+		for (int i = 1; i < logFileList.size(); i++) {
+			List<Integer> descriptionContent = LogFileContent.mapDescription(logFileList.get(i).getDescription());
 			transactionTable.add(descriptionContent);
 			System.out.println(transactionTable);
 		}
-		
+
 		return transactionTable;
-	}
-	
-	public static List<Integer> mapDescription(String description) {
-		List<Integer> descriptionContent = new ArrayList<Integer>();
-		String contentPattern = "[0-9]+";
-		
-		Pattern pattern = Pattern.compile(contentPattern);
-		Matcher matcher = pattern.matcher(description);
-		
-		while(matcher.find()) {
-			descriptionContent.add(Integer.parseInt(matcher.group(0)));
-		}
-		
-		return descriptionContent;
 	}
 
 }
